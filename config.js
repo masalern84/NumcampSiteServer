d32f3f67d9c4aafb35922e49452a2cdc27dcd048
Marco Salerno <masalern84@gmail.com>
2021-04-30 18:50:04 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "1648756488656428",
+    clientSecret: "093809ececb92952943f610e2516be2f",
+  },
 };
